b9dc2cccc1877c8fb3806e3674eff5e38a87a671
binnu-dev <67778157+binnu-dev@users.noreply.github.com>
2025-02-13 14:47:28 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ko">
-      <body className={`${inter.className} bg-gray-50 min-h-screen`}>{children}</body>
+    <html lang="ko" suppressHydrationWarning>
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6920752453203341"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
+      <body className={`${inter.className} min-h-screen bg-gray-50`}>
+        {children}
+      </body>
     </html>
   )
 }
