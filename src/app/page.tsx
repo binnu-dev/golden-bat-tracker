90e8860c1fdbf62c4269de7dad1ee952a48405f6
binnu-dev <67778157+binnu-dev@users.noreply.github.com>
2025-02-13 13:49:13 +0900

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import GoldenBatValue from '@/components/GoldenBatValue';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 bg-gray-50">
+      <div className="container mx-auto">
+        <GoldenBatValue />
+      </div>
+    </main>
+  );
+}
